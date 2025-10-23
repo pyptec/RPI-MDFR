83291a663febe25c65658cb6e16510618d8116ba
Jaime Pedraza <pyptec@hotmail.com>
2025-10-23 16:57:01 -0500

    fix

---
+++ modbusdevices.py
@@ -0,0 +1,85 @@
+import minimalmodbus
+import serial
+import util  # si usas util.get__time_utc() o logging
+
+
+'''
+Parametros del pto serie modbus
+'''
+serialPort= "/dev/ttyS0"
+
+#-----------------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------------
+def payload_event_modbus(config):
+    """
+    Lee temperatura y humedad del sensor THT03R (Modbus RTU) usando la configuración YAML.
+    Espera en config:
+      - slave_id, baudrate, bytesize, parity, stopbits, timeout
+      - opcional: port (si no está, usa variable global serialPort)
+      - registers: lista de dicts con:
+          { "name": "...", "alias": "...", "address": 0, "unit": 1,
+            "fc": 3, "decimals": 1, "signed": false }
+    """
+    """
+    Lee temperatura y humedad del sensor THT03R (Modbus RTU).
+    Compatible con tu YAML reducido (sin fc ni decimals definidos).
+    """
+    valores, unidades = [], []
+    port = config.get('port', None)
+    if port is None:
+        port = serialPort
+    try:
+        #print("\n=== Iniciando lectura THT03R ===")
+        #print(f"Puerto: {serialPort}")
+        #print(f"Slave ID: {config['slave_id']}")
+        #print(f"Baudios: {config['baudrate']}, Paridad: {config['parity']}, Timeout: {config['timeout']}\n")
+        instrumento = minimalmodbus.Instrument(port, config['slave_id'])
+        instrumento.serial.baudrate = config['baudrate']
+        instrumento.serial.bytesize = config['bytesize']
+        instrumento.serial.stopbits = config['stopbits']
+        instrumento.serial.timeout = config['timeout']
+        instrumento.serial.inter_byte_timeout = 0.2
+        instrumento.mode = minimalmodbus.MODE_RTU
+        instrumento.clear_buffers_before_each_transaction = True
+
+        # Paridad
+        parity_map = {
+            'N': serial.PARITY_NONE,
+            'E': serial.PARITY_EVEN,
+            'O': serial.PARITY_ODD
+        }
+        instrumento.serial.parity = parity_map.get(config['parity'].upper(), serial.PARITY_NONE)
+        #instrumento.debug = True
+        # Leer cada registro del sensor
+        for reg in config['registers']:
+            address = reg['address']
+            # valores por defecto para este sensor
+            fc = 3
+            decimals = reg['decimal']
+            signed = False
+            #print(f"→ Leyendo dirección {address} (función {fc}) ...")
+             
+            val = instrumento.read_register(address, decimals, functioncode=fc, signed=signed)
+            #print(f"   Valor leído bruto: {val}")
+            
+            # Redondeo suave
+            val = round(val, 1)
+
+            valores.append(str(val))
+            unidades.append(str(reg['unit']))
+            #print(f"\nValores finales leídos: {valores}")
+            #print(f"Unidades asociadas: {unidades}")
+            #print("==============================\n")
+        return {
+            "d": [{
+                "t": util.get__time_utc(),
+                "g": config['id_device'],
+                "v": valores,
+                "u": unidades
+            }]
+        }
+
+    except Exception as e:
+        util.logging.error(f"Error al leer el THT03R: {e}")
+        return None
--- rpi-mdfr.py
+++ rpi-mdfr.py
@@ -13,12 +13,9 @@ import threading
 import eventHandler
 import shared
 import subprocess
+import modbusdevices
 #import tunel_watcher
 
-'''
-Parametros del pto serie modbus
-'''
-serialPort= "/dev/ttyS0"
 
 # Ruta al archivo .env
 load_dotenv(dotenv_path="/home/pi/.scr/.scr/RPI-MDFR/.env")
@@ -61,167 +58,9 @@ def door_interrupt_callback(channel):
 def leer_float32(instrumento, address):
     registros = instrumento.read_registers(address, 2, functioncode=3)
     return struct.unpack(">f", struct.pack(">HH", registros[0], registros[1]))[0]
-#-----------------------------------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------------------------------------
-def payload_event_CT01CO2(config):
-    valores, unidades = [], []
-    port = config.get('port', None)
-    if port is None:
-        port = serialPort
-    try:
-        instrumento = minimalmodbus.Instrument(port, config['slave_id'])
-        instrumento.serial.baudrate = config['baudrate']
-        instrumento.serial.bytesize = config['bytesize']
-        instrumento.serial.stopbits = config['stopbits']
-        instrumento.serial.timeout = config['timeout']
-        instrumento.mode = minimalmodbus.MODE_RTU
-        instrumento.clear_buffers_before_each_transaction = True
-         # Interpretar la paridad desde el YAML (N, E, O)
-        parity_map = {
-            'N': serial.PARITY_NONE,
-            'E': serial.PARITY_EVEN,
-            'O': serial.PARITY_ODD
-        }
-        instrumento.serial.parity = parity_map.get(config['parity'].upper(), serial.PARITY_NONE)
-     # Leer cada registro del medidor desde la configuración YAML
-        for reg in config['registers']:
-            val = instrumento.read_register(reg['address'], 0, functioncode=3)
-            
-            valores.append(str(round(val, 1)))
-            unidades.append(str(reg['unit']))
-        return {
-            "d": [{
-                "t": util.get__time_utc(),
-                "g": config['id_device'],
-                "v": valores,
-                "u": unidades
-            }]
-        }
 
-    except Exception as e:
-       util.logging.error(f"Error al leer el ct01co2: {e}")
-       return None
-   
 
-#-----------------------------------------------------------------------------------------------------------
-
-#-----------------------------------------------------------------------------------------------------------
-def payload_event_modbus(config):
-    """
-    Lee temperatura y humedad del sensor THT03R (Modbus RTU) usando la configuración YAML.
-    Espera en config:
-      - slave_id, baudrate, bytesize, parity, stopbits, timeout
-      - opcional: port (si no está, usa variable global serialPort)
-      - registers: lista de dicts con:
-          { "name": "...", "alias": "...", "address": 0, "unit": 1,
-            "fc": 3, "decimals": 1, "signed": false }
-    """
-    """
-    Lee temperatura y humedad del sensor THT03R (Modbus RTU).
-    Compatible con tu YAML reducido (sin fc ni decimals definidos).
-    """
-    valores, unidades = [], []
-    port = config.get('port', None)
-    if port is None:
-        port = serialPort
-    try:
-        #print("\n=== Iniciando lectura THT03R ===")
-        #print(f"Puerto: {serialPort}")
-        #print(f"Slave ID: {config['slave_id']}")
-        #print(f"Baudios: {config['baudrate']}, Paridad: {config['parity']}, Timeout: {config['timeout']}\n")
-        instrumento = minimalmodbus.Instrument(port, config['slave_id'])
-        instrumento.serial.baudrate = config['baudrate']
-        instrumento.serial.bytesize = config['bytesize']
-        instrumento.serial.stopbits = config['stopbits']
-        instrumento.serial.timeout = config['timeout']
-        instrumento.serial.inter_byte_timeout = 0.2
-        instrumento.mode = minimalmodbus.MODE_RTU
-        instrumento.clear_buffers_before_each_transaction = True
-
-        # Paridad
-        parity_map = {
-            'N': serial.PARITY_NONE,
-            'E': serial.PARITY_EVEN,
-            'O': serial.PARITY_ODD
-        }
-        instrumento.serial.parity = parity_map.get(config['parity'].upper(), serial.PARITY_NONE)
-        #instrumento.debug = True
-        # Leer cada registro del sensor
-        for reg in config['registers']:
-            address = reg['address']
-            # valores por defecto para este sensor
-            fc = 3
-            decimals = reg['decimal']
-            signed = False
-            #print(f"→ Leyendo dirección {address} (función {fc}) ...")
-             
-            val = instrumento.read_register(address, decimals, functioncode=fc, signed=signed)
-            #print(f"   Valor leído bruto: {val}")
-            
-            # Redondeo suave
-            val = round(val, 1)
-
-            valores.append(str(val))
-            unidades.append(str(reg['unit']))
-            #print(f"\nValores finales leídos: {valores}")
-            #print(f"Unidades asociadas: {unidades}")
-            #print("==============================\n")
-        return {
-            "d": [{
-                "t": util.get__time_utc(),
-                "g": config['id_device'],
-                "v": valores,
-                "u": unidades
-            }]
-        }
-
-    except Exception as e:
-        util.logging.error(f"Error al leer el THT03R: {e}")
-        return None
-#-----------------------------------------------------------------------------------------------------------
-# Función para generar los parámetros del evento
-#-----------------------------------------------------------------------------------------------------------
-def payload_event(config):
-    voltages = []  # Lista para almacenar los valores de voltaje leídos
-    units_list = []  # Lista para almacenar las unidades correspondientes
-    slave_id = config['slave_id']
-    instrumento = minimalmodbus.Instrument(serialPort, slave_id)
-    instrumento.serial.baudrate = config['baudrate']
-    instrumento.serial.bytesize = config['bytesize']
-    instrumento.serial.stopbits = config['stopbits']
-    instrumento.serial.timeout = config['timeout']
-    instrumento.mode = minimalmodbus.MODE_RTU
-    instrumento.clear_buffers_before_each_transaction = True
-    
-     # Interpretar la paridad desde el YAML (N, E, O)
-    parity_map = {
-        'N': serial.PARITY_NONE,
-        'E': serial.PARITY_EVEN,
-        'O': serial.PARITY_ODD
-    }
-    instrumento.serial.parity = parity_map.get(config['parity'].upper(), serial.PARITY_NONE)
-     # Leer cada registro del medidor desde la configuración YAML
-    for reg in config['registers']:
-        try:
-            # Leer el valor flotante de cada registro
-            value = leer_float32(instrumento, reg['address'])
-            voltages.append(str(round(value, 3)))  # Agregar el voltaje leído a la lista
-            units_list.append(str(reg['unit']))  # Agregar la unidad correspondiente a la lista
-        except Exception as e:  # Manejo de excepciones
-            util.logging.error(f"Error al intentar leer el registro: {reg['address']}: {e}")
-            #os.system('sudo reboot')  # Reiniciar si hay error
-
-    # Crear el diccionario con los parámetros del medidor
-    key = config['tipo']
-    params = {
-        "t": util.get__time_utc(),  # Hora en UTC
-        key: config['id_device'],  # Identificador o índice
-        "v": voltages,  # Lista de voltajes leídos
-        "u": units_list  # Lista de unidades
-    }
-    
-    return params
 #-----------------------------------------------------------------------------------------------------------
 # Función que empaqueta el evento en una estructura JSON
 #-----------------------------------------------------------------------------------------------------------
@@ -256,8 +95,6 @@ def process_event_queue():
  #-----------------------------------------------------------------------------------------------------------   
  
  #-----------------------------------------------------------------------------------------------------------    
-def obtener_datos_medidores_y_sensor():
-   import json
 
 def obtener_datos_medidores_y_sensor():
     """
@@ -277,7 +114,7 @@ def obtener_datos_medidores_y_sensor():
                 '/home/pi/.scr/.scr/RPI-MDFR/device/ct01co2.yml',
                 'ct01co2_sensor'
             )
-            medicion_CT01CO2 = payload_event_modbus(config_CT01CO2)
+            medicion_CT01CO2 = modbusdevices.payload_event_modbus(config_CT01CO2)
             if medicion_CT01CO2 is None:
                 util.logging.warning("Sensor CT01CO2 no conectado o sin respuesta.")
                 medicion_CT01CO2 = {
@@ -297,7 +134,7 @@ def obtener_datos_medidores_y_sensor():
                 '/home/pi/.scr/.scr/RPI-MDFR/device/tht03r.yml',
                 'tht03r_sensor'
             )
-            medicion_THT03R = payload_event_modbus(config_THT03R)
+            medicion_THT03R = modbusdevices.payload_event_modbus(config_THT03R)
             
             if medicion_THT03R is None:
                 util.logging.warning("Sensor THT03R no conectado o sin respuesta.")
