9ef5bfebdc894d6930e1888cf9424a0013c41cee
Jaime Pedraza <pyptec@hotmail.com>
2025-11-04 06:50:20 -0500

    ajuste start

--- start_medidor.sh
+++ start_medidor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Activar entorno virtual
-source /home/pi/SAMEE200/bin/activate
+source /home/pi/.scr/bin/activate
 
 # Esperar 5 segundos antes de ejecutar el script en Python
 sleep 5
 
 # Ejecutar el script en Python
-python /home/pi/SAMEE200/scr/pypmedidores.py
+python /home/pi/.scr/.scr/RPI-MDFR/rpi-mdfr.py
